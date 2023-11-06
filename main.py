2b0341f39902698876e4897b8b2781219b702c49
miralistyacahya <18221116@std.stei.itb.ac.id>
2023-11-07 01:30:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,21 +15,5 @@ app.include_router(orderRouter)
 async def home():
     print("Server is running")
 
-railway_db_config = {
-    "host": "viaduct.proxy.rlwy.net",
-    "port": 35170,
-    "user": "root",
-    "password": "632BfeAd5EGD3dF5bBhbbh6CB62bCeb6",
-    "database": "railway"
-}
-
-try:
-    conn = mysql.connector.connect(**railway_db_config)
-    print("Success connect to db")
-except mysql.connector.Error as e:
-    print(e)
-else:
-    cursor = conn.cursor()
-
 if __name__ == "__main__" :
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
