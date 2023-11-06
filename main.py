3487bf0ac94a5012603303f72d97225f93cb3066
miralistyacahya <18221116@std.stei.itb.ac.id>
2023-11-06 22:57:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ app.include_router(cakeRouter)
 app.include_router(customerRouter)
 app.include_router(orderRouter)
 
-if __name__ == "__main__" :
-    uvicorn.run("main:app", host="0.0.0.0", port=8600, reload=True)
+# if __name__ == "__main__" :
+#     uvicorn.run("main:app", host="0.0.0.0", port=8600, reload=True)
